5ec6ba23f222ee26f943a2aaf2af0b4ced43cf21
Roberto Sillas <65824725+Robertosga@users.noreply.github.com>
2023-09-11 13:14:48 -0300

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -2,30 +2,32 @@ import tkinter as tk
 from tkinter import filedialog
 from fpdf import FPDF
 import libperg
-import lib_func
+import lib_func2
+
+
 
 # Função para inserir e exibir nome do jogador
-lib_func.exibir_nome_jogador()
+lib_func2.exibir_nome_jogador()
 
 # Função para exibir resultado
-lib_func.exibir_resultado()
+lib_func2.exibir_resultado()
 
 
 # Função para girar a roleta
-lib_func.girar_roleta()
+lib_func2.girar_roleta()
 
 # Função para exibir uma pergunta aleatória
-lib_func.exibir_pergunta_aleatoria()
+lib_func2.exibir_pergunta_aleatoria()
 
 
 # Função para exibir uma pergunta 
-lib_func.exibir_pergunta("pergunta", "opcoes", "resposta", "imagem")
+lib_func2.exibir_pergunta("pergunta", "opcoes", "resposta", "imagem")
 
 
 # Função para exibir uma mensagem na tela
-lib_func.exibir_mensagem("mensagem","cor")
+lib_func2.exibir_mensagem("mensagem","cor")
 
 # Função para salvar pontuação
-lib_func.salvar_pontuacao("nome_jogador", "pontuacao_total")
+lib_func2.salvar_pontuacao("nome_jogador", "pontuacao_total")
 
 
